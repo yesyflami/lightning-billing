0cc028abe9c2a71b05e06904daa2d3b7be8cf80a
yesyflami <yesy9379@163.com>
2020-09-28 14:49:55 +0800

    deploy

---
+++ precache-manifest.f33ea96f0d0b70acc0053b04876feadb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a796a5a522d667024bbda1c13110696",
+    "url": "/lightning-billing/QRcode.png"
+  },
+  {
+    "revision": "5359d9d1617df64321db",
+    "url": "/lightning-billing/css/app.1a10f3c1.css"
+  },
+  {
+    "revision": "b97556003273a6e9bba792c261ddeca5",
+    "url": "/lightning-billing/index.html"
+  },
+  {
+    "revision": "5359d9d1617df64321db",
+    "url": "/lightning-billing/js/app.02231a6b.js"
+  },
+  {
+    "revision": "7716c195ccd91595b01d",
+    "url": "/lightning-billing/js/chunk-vendors.4e887dfd.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/lightning-billing/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/lightning-billing/robots.txt"
+  }
+]);

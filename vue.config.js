c6b43bbe3b3b24ebc99c88746b37a4ca09cc85da
yesyflami <yesy9379@163.com>
2020-09-28 14:28:38 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require("path")
 
 module.exports = {
     publicPath: process.env.NODE_ENV === "production"
-        ? "/lightningBilling/"
+        ? "/lightning-billing/"
         : "/",
     lintOnSave: false,
     chainWebpack: config => {
